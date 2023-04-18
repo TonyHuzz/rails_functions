7a38a99263c09fca8ca12f6d3f52e64b9963e504
tonyhu <benabcd5779@gmail.com>
2023-04-18 20:03:47 +0800

    no message

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -269,7 +269,6 @@ Devise.setup do |config|
   config.sign_out_via = :delete
 
 
-  Dotenv::Railtie.load
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
