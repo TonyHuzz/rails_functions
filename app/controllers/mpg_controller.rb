f27615f3e1c7185dd593271cef33d661d12cf111
tonyhu <benabcd5779@gmail.com>
2023-04-19 16:23:23 +0800

    no message

--- app/controllers/mpg_controller.rb
+++ app/controllers/mpg_controller.rb
@@ -11,13 +11,13 @@ class MpgController < ApplicationController
     amt = 100
     itemDesc = "金流串接test"
     hashKey = '0YInfrMwAlOxmtvzM6zFzmcqw5zKfAAa'
-    hashIv = 'CAH4OMANQgPLuYXP'
+    hashIV = 'CAH4OMANQgPLuYXP'
 
     data = "MerchantID=#{merchantID}&RespondType=#{respondType}&TimeStamp=#{timeStamp}&Version=#{version}&MerchantOrderNo=#{merchantOrderNo}&Amt=#{amt}&ItemDesc=#{itemDesc}"
 
     data = addpadding(data)
-    aes = encrypt_data(data, hashKey, hashIv, 'AES-256-CBC')
-    checkValue = "HashKey=#{hashKey}&#{aes}&HashIV=#{hashIv}"
+    aes = encrypt_data(data, hashKey, hashIV, 'AES-256-CBC')
+    checkValue = "HashKey=#{hashKey}&#{aes}&HashIV=#{hashIV}"
 
     @merchantID = merchantID
     @tradeInfo = aes
@@ -26,11 +26,65 @@ class MpgController < ApplicationController
   end
 
   def notify
-    Logger.new("#{Rails.root}/notify.log").try("info", params)
+    hashKey = '0YInfrMwAlOxmtvzM6zFzmcqw5zKfAAa'
+    hashIV = 'CAH4OMANQgPLuYXP'
+
+    if params["Status"] == "SUCCESS"
+
+      tradeInfo = params["TradeInfo"]
+      tradeSha = params["TradeSha"]
+
+      checkValue = "HashKey=#{hashKey}&#{tradeInfo}&HashIV=#{hashIV}"
+      if tradeSha == Digest::SHA256.hexdigest(checkValue).upcase
+        rawTradeInfo = decrypt_data(tradeInfo, hashKey, hashIV, 'AES-256-CBC')
+        result = JSON.parse(rawTradeInfo)
+        Logger.new("#{Rails.root}/notify.log").try("info", result)
+      end
+    end
+
+    respond_to do |format|
+      format.json {render json: {result: "success"}}
+    end
   end
 
   def return
-    Logger.new("#{Rails.root}/return.log").try("info", params)
+    hashKey = '0YInfrMwAlOxmtvzM6zFzmcqw5zKfAAa'
+    hashIV = 'CAH4OMANQgPLuYXP'
+
+    if params["Status"] == "SUCCESS"
+
+      tradeInfo = params["TradeInfo"]
+      tradeSha = params["TradeSha"]
+
+      checkValue = "HashKey=#{hashKey}&#{tradeInfo}&HashIV=#{hashIV}"
+      if tradeSha == Digest::SHA256.hexdigest(checkValue).upcase
+        rawTradeInfo = decrypt_data(tradeInfo, hashKey, hashIV, 'AES-256-CBC')
+        result = JSON.parse(rawTradeInfo)
+        Logger.new("#{Rails.root}/return.log").try("info", result)
+      end
+    end
+
+    redirect_to :root
+  end
+
+  def customer
+    hashKey = '0YInfrMwAlOxmtvzM6zFzmcqw5zKfAAa'
+    hashIV = 'CAH4OMANQgPLuYXP'
+
+    if params["Status"] == "SUCCESS"
+
+      tradeInfo = params["TradeInfo"]
+      tradeSha = params["TradeSha"]
+
+      checkValue = "HashKey=#{hashKey}&#{tradeInfo}&HashIV=#{hashIV}"
+      if tradeSha == Digest::SHA256.hexdigest(checkValue).upcase
+        rawTradeInfo = decrypt_data(tradeInfo, hashKey, hashIV, 'AES-256-CBC')
+        result = JSON.parse(rawTradeInfo)
+        Logger.new("#{Rails.root}/customer.log").try("info", result)
+      end
+    end
+
+    redirect_to :root
   end
 
 
@@ -50,6 +104,26 @@ class MpgController < ApplicationController
     cipher.iv = iv
     encrypted = cipher.update(data) + cipher.final
     return encrypted.unpack("H*")[0].upcase
+  end
+
+  def removedPadding(data)
+    blocksize = 32
+    loop do
+      lastHex = data.last.bytes.first
+      break if lastHex >= blocksize
+      data = data[0...-1]
+    end
+    return data
+  end
+
+  def decrypt_data(data, key, iv, cipher_type)
+    cipher = OpenSSL::Cipher.new(cipher_type)
+    cipher.decrypt
+    cipher.key = key
+    cipher.iv = iv
+    packedData = [data.downcase].pack('H*')
+    data = removedPadding(cipher.update(packedData))
+    return data + cipher.final
   end
 
 end
